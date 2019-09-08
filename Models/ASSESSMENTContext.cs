2dd3aeef3d4b405060c0121024fd2d9ca53736f2
genious420 <53940628+genious420@users.noreply.github.com>
2019-09-08 14:08:50 +0500

    connect with database

---
+++ Models/ASSESSMENTContext.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication1.Models
+{
+    public partial class ASSESSMENTContext : DbContext
+    {
+        public ASSESSMENTContext()
+        {
+        }
+
+        public ASSESSMENTContext(DbContextOptions<ASSESSMENTContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<PostTable> PostTable { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-KBDTL9C;Database=ASSESSMENT;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("category");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("created_by")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("created_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Image)
+                    .HasColumnName("image")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("modified_by")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modified_date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Status)
+                    .HasColumnName("status")
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<PostTable>(entity =>
+            {
+                entity.ToTable("post_table");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CategoryId).HasColumnName("category_id");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("created_by")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.CreatedData)
+                    .HasColumnName("created_data")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Descripition)
+                    .HasColumnName("descripition")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Image)
+                    .HasColumnName("image")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.MetaDescripition)
+                    .HasColumnName("meta_descripition")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.MetaKeywords)
+                    .HasColumnName("meta_keywords")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.MetaTitle)
+                    .HasColumnName("meta_title")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("modified_by")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modified_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Status)
+                    .HasColumnName("status")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.Titlle)
+                    .HasColumnName("titlle")
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
---
+++ Models/PostTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class PostTable
+    {
+        public int Id { get; set; }
+        public int? CategoryId { get; set; }
+        public string Titlle { get; set; }
+        public string Descripition { get; set; }
+        public string Image { get; set; }
+        public string MetaKeywords { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescripition { get; set; }
+        public string Status { get; set; }
+        public DateTime? CreatedData { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
